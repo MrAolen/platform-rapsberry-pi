7629d9befda130d3f1074d6853d2b9e4391e2780
gunthernic <gunthernic@eisti.eu>
2017-04-25 23:37:05 +0200

    Delete all project and create command executor

--- workspace/bluetooth/src/main/java/com/gunther/rapsberry/bluetooth/bluetooth/App.java
+++ workspace/bluetooth/src/main/java/com/gunther/rapsberry/bluetooth/bluetooth/App.java
@@ -1,41 +1,10 @@
 package com.gunther.rapsberry.bluetooth.bluetooth;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.bluetooth.DiscoveryAgent;
-import javax.bluetooth.LocalDevice;
-import javax.bluetooth.RemoteDevice;
-
-/**
- * Hello world!
- *
- */
 public class App {
-	public static Object lock = new Object();
-	public static List<RemoteDevice> devices = new ArrayList<RemoteDevice>();
-
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		ShellCommandExecutor executor = new ShellCommandExecutor();
 
-		try {
-			LocalDevice localDevice = LocalDevice.getLocalDevice();
-
-			DiscoveryAgent agent = localDevice.getDiscoveryAgent();
-
-			agent.startInquiry(DiscoveryAgent.GIAC, new MyDiscoveryListener());
-
-			try {
-				synchronized (lock) {
-					lock.wait();
-				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			System.out.println("Device Inquiry Completed. ");
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		String output = executor.executeCommand(ShellCommandExecutor.SHELL_START_BLUETOOTH);
+		System.out.println("Resultat : " + output + " ! ");
 	}
 }
